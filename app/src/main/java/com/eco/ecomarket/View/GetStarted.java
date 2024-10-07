fc667be1a67a3e28897d63284e262faefa05bc64
Nargiza <kuzybahovanargiza@gmail.com>
2024-10-07 13:16:50 +0500

    project first v3

--- app/src/main/java/com/eco/ecomarket/View/GetStarted.java
+++ app/src/main/java/com/eco/ecomarket/View/GetStarted.java
@@ -1,4 +1,4 @@
-package com.eco.ecomarket;
+package com.eco.ecomarket.View;
 
 import android.os.Bundle;
 
@@ -8,13 +8,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.eco.ecomarket.R;
+
+public class GetStarted extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_get_started);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- app/src/main/java/com/eco/ecomarket/View/NavigationActivity.java
+++ app/src/main/java/com/eco/ecomarket/View/NavigationActivity.java
@@ -1,9 +1,8 @@
-package com.eco.ecomarket;
+package com.eco.ecomarket.View;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Html;
-import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -14,6 +13,7 @@ import androidx.cardview.widget.CardView;
 import androidx.viewpager.widget.ViewPager;
 
 import com.eco.ecomarket.Adapter.OnBoardingAdapter;
+import com.eco.ecomarket.R;
 
 public class NavigationActivity extends AppCompatActivity {
 
